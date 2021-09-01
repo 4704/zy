9178aaa70a2fc69aeee087d059970c73816aa4f0
lishibo <470499826@qq.com>
2021-09-01 14:54:44 +0800

    git commit -m 课程

--- app/src/views/ShowTeachers/ShowTeachers.vue
+++ app/src/views/ShowTeachers/ShowTeachers.vue
@@ -1,45 +1,88 @@
 <template>
   <div class="dt">
-    <div>
-      <van-card
-        v-for="(item, index) in listtt"
-        :key="index"
-        :desc="item.introduction"
-        :title="item.teacher_name"
-        :thumb="item.teacher_avatar"
-      />
-    </div>
+    
+      <div class="teacher">
+        <div class="t-next" v-for="(item,index) in listtt.list" :key="index">
+          <div class="next-box">
+            <div class="box-left">
+              <img :src="item.teacher_avatar" alt="">
+            </div>
+            <div class="box-right">
+              <p class="right-p1">{{item.teacher_name}}</p>
+              <p class="right-p2">{{item.introduction}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    
   </div>
 </template>
 <script>
-import { shuj } from "@/http/api";
+  import {
+    shuj
+  } from "@/http/api";
 
-export default {
-  data() {
-    return {
-      listtt: [],
-    };
-  },
-  mounted() {
-    this.getlist();
-  },
-  methods: {
-    async getlist() {
-      let {
-        data: { data: res },
-      } = await shuj();
-      this.listtt = res[2].list;
-      console.log(this.listtt);
+  export default {
+    data() {
+      return {
+        listtt: [],
+      };
     },
-  },
-};
+    mounted() {
+      this.getlist();
+    },
+    methods: {
+      async getlist() {
+        let { data: res } = await shuj();
+        this.listtt = res[2]
+      },
+    },
+  };
 </script>
-<style scoped>
-.dt {
-  width: 100%;
-  height: 100%;
-  background: #f7f8fa;
-}
-.van-card{
-margin-top: 5px;}
+<style lang="scss" scoped>
+ .teacher{
+          width: 90vw;
+          margin: auto;
+          margin-bottom: 120px;
+          .t-next{
+              width: 100%;
+              height: 100px;
+              background-color: #999;
+              border-radius: 20px;
+              margin: 20px 0;
+              position: relative;
+              background: #f7f8fa;
+              .next-box{
+                  width: 80%;
+                  height: 60%;
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  transform: translate(-50%,-50%);
+                  display: flex;
+                  justify-content: space-between;
+                  .box-left{
+                      flex: 1;
+                      img{
+                          width: 50px;
+                          height: 50px;
+                          display: block;
+                          border-radius: 50%;
+                      }
+                  }
+                  .box-right{
+                      flex: 5;
+                      margin-left: 20px;
+                      .right-p1{
+                          font-size: 20px;
+                      }
+                      .right-p2{
+                          font-size: 15px;
+                          margin-top: 10px;
+                          color: #ccc;
+                      }
+                  }
+              }
+          }
+      }
 </style>
